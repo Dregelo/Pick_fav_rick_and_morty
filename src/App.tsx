1fa83833d6b9bbfa266bc947750f124619caa148
PeterR <solutionsgelo@gmail.com>
2019-07-28 09:35:41 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Rick and Morty</h1>
+      <p>Pick your favourite episode!</p>
+    </div>
+  );
+};
+
+export default App;
